d5acdc379a98d384d61e2079f4c99829ad40417d
GauriDBandbe <96105708+GauriDBandbe@users.noreply.github.com>
2021-12-21 11:18:53 +0530

    Add files via upload

---
+++ Angular/Day1/ProductList.ts
@@ -0,0 +1,14 @@
+export class ProductList
+{
+  ProdID:number = 0;
+  ProdName:string  = "";
+  ProdPrice:number = 0;
+  ProdCategory:string  = "";
+  ProdQty:number = 0;
+}
+export class TechnogolyList
+{
+  TechName:string  = "";
+  TechImg:string  = "";
+  TechDesc:string  = "";
+}
